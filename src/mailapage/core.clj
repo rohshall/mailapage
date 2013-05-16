fca79d0f04f8025695f071e0a2fdd68b0790285c
Salil Wadnerkar <rohshall@gmail.com>
2013-05-17 00:35:30 +0800

    Added reading directory and applying pdf conversion

--- src/mailapage/core.clj
+++ src/mailapage/core.clj
@@ -1,6 +1,28 @@
 (ns mailapage.core
-  (:gen-class))
+  (:gen-class)
+  (:import (java.io File BufferedWriter OutputStreamWriter FileOutputStream IOException)
+           (org.apache.pdfbox.pdmodel PDDocument)
+           (org.apache.pdfbox.util PDFTextStripper)))
+
+
+; the main workhorse function that does the conversion
+(defn convert-to-text [dirName fileName]
+  (let [src (str dirName "/" fileName)
+        dest (str src ".txt")
+        stripper (PDFTextStripper.)]
+    (with-open [pd (PDDocument/load (File. src))
+                wr (BufferedWriter. (OutputStreamWriter. (FileOutputStream. (File. dest))))]
+      (println src "pages:" (.getNumberOfPages pd))
+      (.writeText stripper pd wr))))
+
 
 (defn -main [& args]
-  (println "Hello World"))
+  (if args
+    (let [dirName (first args)
+          dir (File. dirName)]
+      ; get the pdf files in the directory and apply the conversion
+      (if (.isDirectory dir)
+        (doseq [fileName (.list dir)]
+          (if (.endsWith fileName ".pdf")
+            (convert-to-text dirName fileName)))))))
 
